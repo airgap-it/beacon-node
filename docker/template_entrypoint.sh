f6e9dba59bb24c942ab94ace176ae7712ba50b21
Mike Godenzi <m.godenzi@papers.ch>
2021-01-18 15:08:50 +0100

    chore(): added dev-1 and dev-2 deployments

--- docker/template_entrypoint.sh
+++ docker/template_entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" /config/homeserver.yaml
-sed -i "s/{{DB_HOST}}/$DB_HOST/g" /config/homeserver.yaml
-sed -i "s/{{DB_USER}}/$DB_USER/g" /config/homeserver.yaml
-sed -i "s/{{DB_PASS}}/$DB_PASS/g" /config/homeserver.yaml
-sed -i "s/{{DB_NAME}}/$DB_NAME/g" /config/homeserver.yaml
+HS_PATH="/config/$HS_FILE_NAME"
+sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" $HS_PATH
+sed -i "s/{{DB_HOST}}/$DB_HOST/g" $HS_PATH
+sed -i "s/{{DB_USER}}/$DB_USER/g" $HS_PATH
+sed -i "s/{{DB_PASS}}/$DB_PASS/g" $HS_PATH
+sed -i "s/{{DB_NAME}}/$DB_NAME/g" $HS_PATH
 
-python -m synapse.app.homeserver --config-path /config --keys-directory /data --generate-keys
+python -m synapse.app.homeserver --config-path $HS_PATH --keys-directory /data --generate-keys
 /usr/local/bin/wait-for.sh $DB_HOST:5432 -- python -m synapse.app.homeserver --config-path /config --keys-directory /data

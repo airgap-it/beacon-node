ebf7d8d6af7871df5180fc67ced3face9eb8bc9a
Mike Godenzi <m.godenzi@papers.ch>
2021-01-18 15:49:25 +0100

    fix(): fixed server name and template_entrypoint.sh script

--- docker/template_entrypoint.sh
+++ docker/template_entrypoint.sh
@@ -7,4 +7,4 @@ sed -i "s/{{DB_PASS}}/$DB_PASS/g" $HS_PATH
 sed -i "s/{{DB_NAME}}/$DB_NAME/g" $HS_PATH
 
 python -m synapse.app.homeserver --config-path $HS_PATH --keys-directory /data --generate-keys
-/usr/local/bin/wait-for.sh $DB_HOST:5432 -- python -m synapse.app.homeserver --config-path /config --keys-directory /data
+/usr/local/bin/wait-for.sh $DB_HOST:5432 -- python -m synapse.app.homeserver --config-path $HS_PATH --keys-directory /data

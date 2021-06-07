b9fb4fec7b682f449359335a3efce92a78d6bbbe
Lionel Perrin <l.perrin@papers.ch>
2021-06-07 15:47:14 +0200

    chore(docker): fix worker config

--- docker/synctl_entrypoint.sh
+++ docker/synctl_entrypoint.sh
@@ -10,9 +10,11 @@ sed -i "s/{{SIGNING_KEY}}/$SIGNING_KEY/g" /config/signing.key
 /usr/local/bin/wait-for.sh $DB_HOST:5432
 
 # synctl start /config/homeserver.yaml 
-synctl start /config/shared_config.yaml.yaml -w /config/workers/worker1.yaml
-synctl start /config/shared_config.yaml -w /config/workers/worker2.yaml
-synctl start /config/homeserver.yaml -w /config/workers/main_process.yaml --no-daemonize
+# synctl start /config/shared_config.yaml.yaml -w /config/workers/worker1.yaml
+# synctl start /config/shared_config.yaml -w /config/workers/worker2.yaml
+synctl start /config/homeserver.yaml -w /config/workers/main_process.yaml
+synctl start /config/homeserver.yaml -w /config/workers/woker1.yaml
+synctl start /config/homeserver.yaml -w /config/workers/woker2.yaml --no-daemonize
 # systemctl daemon-reload
 # systemctl start matrix-synapse.service
 # systemctl enable matrix-synapse-worker@woker1.service

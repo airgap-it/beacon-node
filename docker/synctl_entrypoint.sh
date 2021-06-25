119e4889bfff176409f97672b8662968da42b7bd
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-06-25 13:44:04 +0200

    feat(workers): use 20 workers

--- docker/synctl_entrypoint.sh
+++ docker/synctl_entrypoint.sh
@@ -16,7 +16,23 @@ synctl start /config/homeserver.yaml -w /config/workers/main_process.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker1.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker2.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker3.yaml
-synctl start /config/homeserver.yaml -w /config/workers/worker4.yaml --no-daemonize
+synctl start /config/homeserver.yaml -w /config/workers/worker4.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker5.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker6.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker7.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker8.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker9.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker10.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker11.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker12.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker13.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker14.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker15.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker16.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker17.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker18.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker19.yaml
+synctl start /config/homeserver.yaml -w /config/workers/worker20.yaml --no-daemonize
 # systemctl daemon-reload
 # systemctl start matrix-synapse.service
 # systemctl enable matrix-synapse-worker@woker1.service

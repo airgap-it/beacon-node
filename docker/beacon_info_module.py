6df5f985ae7b46e8609695a92a9a48aa847bdf9d
Lionel Perrin <l.perrin@papers.ch>
2022-09-06 16:31:20 +0200

    feat(region_api): add region api

---
+++ docker/beacon_info_module.py
@@ -0,0 +1,23 @@
+import json
+from twisted.web.resource import Resource
+import logging
+import time
+import os
+
+logger = logging.getLogger(__name__)
+
+class BeaconInfoModule(Resource):
+    def __init__(self, config, api):
+        super().__init__()
+        logger.info(f"init called with config: {config}")
+        self.config = config
+        self.api = api
+        self.api.register_web_resource(path="/_synapse/client/beacon/info", resource=self)
+
+    def render(self, request):
+        request.setHeader(b"content-type", b"application/json; charset=utf-8")
+        return json.dumps({
+            "region":os.environ.get("SERVER_REGION", "region not set"),
+            "known_servers":self.config.get("known_servers",[]),
+            "timestamp": time.time()
+            }).encode("utf-8")

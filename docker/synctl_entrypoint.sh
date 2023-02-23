d0343160f677aa6e5c0201c12788de73a83abb88
Lionel Perrin <l.perrin@papers.ch>
2023-02-23 16:20:57 +0100

    Update docker/synctl_entrypoint.sh

--- docker/synctl_entrypoint.sh
+++ docker/synctl_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" /config/homeserver.yaml
-sed -i "s/{{SERVER_NAME}}/$SERVER_NAME/g" /config/shared_config.yaml
-sed -i "s/{{DB_HOST}}/$DB_HOST/g" /config/homeserver.yaml
-sed -i "s/{{DB_USER}}/$DB_USER/g" /config/homeserver.yaml
-sed -i "s/{{DB_PASS}}/$DB_PASS/g" /config/homeserver.yaml
-sed -i "s/{{DB_NAME}}/$DB_NAME/g" /config/homeserver.yaml
-sed -i "s/{{SIGNING_KEY}}/$SIGNING_KEY/g" /config/signing.key
+sed -i "s|{{SERVER_NAME}}|$SERVER_NAME|g" /config/homeserver.yaml
+sed -i "s|{{SERVER_NAME}}|$SERVER_NAME|g" /config/shared_config.yaml
+sed -i "s|{{DB_HOST}}|$DB_HOST|g" /config/homeserver.yaml
+sed -i "s|{{DB_USER}}|$DB_USER|g" /config/homeserver.yaml
+sed -i "s|{{DB_PASS}}|$DB_PASS|g" /config/homeserver.yaml
+sed -i "s|{{DB_NAME}}|$DB_NAME|g" /config/homeserver.yaml
+sed -i "s|{{SIGNING_KEY}}|$SIGNING_KEY|g" /config/signing.key
 
 /usr/local/bin/wait-for.sh $DB_HOST:5432
 

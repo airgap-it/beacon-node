d9bdc27e0f73107bb463ec74b438613e0ac8da45
Mike Godenzi <m.godenzi@papers.ch>
2026-02-27 11:08:40 +0100

    chore: updated to synapse v1.148.0

--- docker/synctl_entrypoint.sh
+++ docker/synctl_entrypoint.sh
@@ -8,16 +8,8 @@ sed -i "s/{{DB_NAME}}/$DB_NAME/g" /config/homeserver.yaml
 echo "${SIGNING_KEY}" > /config/signing.key
 /usr/local/bin/wait-for.sh $DB_HOST:5432
 
-# synctl start /config/homeserver.yaml 
-# synctl start /config/shared_config.yaml.yaml -w /config/workers/worker1.yaml
-# synctl start /config/shared_config.yaml -w /config/workers/worker2.yaml
 synctl start /config/homeserver.yaml -w /config/workers/main_process.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker1.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker2.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker3.yaml
 synctl start /config/homeserver.yaml -w /config/workers/worker4.yaml --no-daemonize
-# systemctl daemon-reload
-# systemctl start matrix-synapse.service
-# systemctl enable matrix-synapse-worker@woker1.service
-# systemctl enable matrix-synapse-worker@woker2.service
-# systemctl start matrix-synapse.target.service
